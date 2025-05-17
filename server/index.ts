ae2d7016955ad12cf82262fd6bb93f2ec80bd0d4
Dương Xuân Bách <bachduong7103@gmail.com>
2025-05-17 22:30:55 +0700

    add

--- server/index.ts
+++ server/index.ts
@@ -75,8 +75,7 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "0.0.0.0"
   }, () => {
       console.log(`Server is now listening on port ${port}`);
     log(`serving on port ${port}`);
